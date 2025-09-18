ad5c7050606dcacf98023c5c020d8d3e7704f6fe
tanin-t <t.thanasopon@gmail.com>
2025-09-18 23:01:05 +0700

    Setup gluestack ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Button, ButtonSpinner, ButtonText } from '@/components/ui/button';
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,10 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button className="bg-red-500 hover:bg-orange-400" variant="solid" size="md" onPress={() => {console.log("hello")}}>
+        <ButtonSpinner color="white" />
+        <ButtonText>Get Started</ButtonText>
+      </Button>
     </View>
   );
 }
