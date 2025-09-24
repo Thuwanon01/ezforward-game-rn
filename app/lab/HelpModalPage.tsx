b7e3ef8077607fe8052441721c9c8b2721066f86
ningchuengtao <ningchuengtao@gmail.com>
2025-09-24 22:45:57 +0700

    NingchuengLab

---
+++ app/lab/HelpModalPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HelpModalPage = () => {
+    return (
+        <View>
+            <Text>HelpModalPage</Text>
+        </View>
+    )
+}
+
+export default HelpModalPage
+
+const styles = StyleSheet.create({})
