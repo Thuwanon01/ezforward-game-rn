6bd94a19db8cead85edeee49cf6a7eb1fb3e2f49
FrongKub <rattanasangnak@gmail.com>
2025-09-24 22:46:36 +0700

    test

---
+++ app/lab/ChoiceBox.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ChoiceBox() {
+  return (
+    <View>
+      <Text>ChoiceBox</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
