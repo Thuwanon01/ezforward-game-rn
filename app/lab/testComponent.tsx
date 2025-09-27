b41a1ac01c7f945e7d9b4f3f658cba545cfa0805
Bingza <tuwanon.att@gmail.com>
2025-09-27 09:09:07 +0700

    Bing: 1st draft explanation

--- app/lab/testComponent.tsx
+++ app/lab/testComponent.tsx
@@ -1,20 +1,40 @@
 
-
-import React from 'react';
+import ExplanationPanel from '@/components/lab/ExplanationPanel';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 
+
 export default function testComponent() {
+    const [helperStatus, setHelperStatus] = useState({"eliminate":false,"double":false,"change":false})
+    const [explanationStatus, setExplanationStatus] = useState(true)
+    const [correctAnswer, setCorrectAnswer] = useState("A. will take")
+    const [correctExplanation, setCorrectExplanation] = useState("Good answer this is explanation for the correct answer, Like like like for long")
+    const [incorrectAnswer, setIncorrectAnswer] = useState("B. is taking")
+    const [incorrectExplanation, setIncorrectExplanation] = useState("Also Good answer but this is explanation for the incorrect answer, Like like like for long")
+    const [explanation, setExplanation] = useState("Just explanation for plain text hahahahahahahahahaha")
+
 
     return (
         <View className='flex-1'>
-            
-            
+
+
             <View className='flex-1'>
                 <Text>testComponent</Text>
-            </View>
-            <View className='bg-[#27548A] h-[64] rounded-t-xl'>
 
             </View>
+            <ExplanationPanel 
+                correctAnswer={correctAnswer} 
+                correctExplanation={correctExplanation} 
+                incorrectAnswer={incorrectAnswer} 
+                incorrectExplanation={incorrectExplanation} 
+                explanation={explanation} 
+                helperStatus={helperStatus} 
+                explanationStatus={explanationStatus} />
+            
+            
+            
+
+
 
         </View>
     )
--- components/lab/explanationPanel.tsx
+++ components/lab/explanationPanel.tsx
@@ -1,12 +1,85 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, ButtonText } from '@/components/ui/button';
+import { Image } from 'expo-image';
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 
-export default function explanationPanel() {
+interface Props {
+  correctAnswer: string
+  correctExplanation: string
+  incorrectAnswer: string
+  incorrectExplanation: string
+  explanation: string
+  helperStatus: { "eliminate": boolean, "double": boolean, "change": boolean }
+  explanationStatus: boolean
+
+}
+
+export default function ExplanationPanel({ correctAnswer, correctExplanation, incorrectAnswer,
+  incorrectExplanation, explanation, helperStatus, explanationStatus }: Props) {
+    const [openExplanation, setOpenExplanation] = useState(false)
+
+    const toggleExplanation = () =>{
+      openExplanation ? setOpenExplanation(false) : setOpenExplanation(true)
+    }
+
+
   return (
-    <View>
-      <Text>explanationPanel</Text>
+    //All box of footer
+    <View className='bg-[#27548A] rounded-t-xl'>
+
+      {/* Condition to show pop-up arrow */}
+      {explanationStatus && <View className="flex-row justify-center mt-[12]">
+        <TouchableOpacity onPress={toggleExplanation}>
+          {openExplanation ? <Image source={require('assets/images/downArrow.svg')} style={{ width: 28, height: 28 }} /> : 
+          <Image source={require('assets/images/upArrow.svg')} style={{ width: 28, height: 28 }} />}
+        </TouchableOpacity>
+      </View>}
+
+      {/* Text explanation */}
+      {openExplanation && <View className="m-[16]">
+        <View>
+          <Text className="text-red-700">{correctAnswer}</Text>
+          <Text className="text-white">{`  - ${correctExplanation}`}</Text>
+          <Text className="text-green-700">{incorrectAnswer}</Text>
+          <Text className="text-white">{`  - ${incorrectExplanation}`}</Text>
+          <View className='h-[8]'></View>
+          <Text className="text-white font-bold ">{explanation}</Text>
+        </View>
+      </View>}
+      
+
+      {/* Icon and button  */}
+      <View className="flex-row justify-between m-[16]">
+        <TouchableOpacity onPress={() => alert('heyyyy')}>
+          <Image
+            source={require('assets/images/Vector.svg')}
+            style={{ width: 28, height: 28, marginLeft: 32 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert('two')}>
+          <Image
+            source={require('assets/images/double.svg')}
+            style={{ width: 28, height: 28 }} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert('xxxxxxxx')}>
+          <Image
+            source={require('assets/images/changeChoice.svg')}
+            style={{ width: 28, height: 28 }} />
+        </TouchableOpacity>
+        <Button
+          variant="solid"
+          size="lg"
+          action="primary"
+          className="mr-[32] bg-[#FCC61D] rounded-3xl px-[32]">
+          <ButtonText className='text-white font-bold text-3xl'>Next</ButtonText>
+        </Button>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+
+
+{/* <Image source={require('assets/images/downArrow.svg')} style={{width:28, height:28 }} />
+                <Image source={require('assets/images/upArrow.svg')} style={{width:28, height:28 }} /> */}
+
